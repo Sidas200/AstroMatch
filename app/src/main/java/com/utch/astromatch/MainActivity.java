7762d92ad546dff100b0e91cc69614547c97d136
Arturo Sidas <arturo-sidas@hotmail.com>
2024-11-12 10:31:05 -0600

    Conectado a firebase pero tiene erorres

--- app/src/main/java/com/utch/astromatch/MainActivity.java
+++ app/src/main/java/com/utch/astromatch/MainActivity.java
@@ -1,20 +1,31 @@
 package com.utch.astromatch;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -23,6 +34,51 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+            Map<String, Object> Usuarios = new HashMap<>();
+            User.put("first", "Ada");
+            User.put("last", "Lovelace");
+            User.put("born", 1815);
+
+// Add a new document with a generated ID
+            db.collection("users")
+                    .add(User)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error adding document", e);
+                        }
+                    });
+            // Create a new user with a first, middle, and last name
+            Map<String, Object> Usuarios = new HashMap<>();
+            User.put("first", "Alan");
+            User.put("middle", "Mathison");
+            User.put("last", "Turing");
+            User.put("born", 1912);
+
+// Add a new document with a generated ID
+            db.collection("users")
+                    .add(User)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error adding document", e);
+                        }
+                    });
+
+
         });
     }
 
